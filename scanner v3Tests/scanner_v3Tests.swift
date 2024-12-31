43241833e73f4ae2153e9899613468e7991af853
andrew bell <andybellmyoflex@me.com>
2024-12-31 14:45:31 +0400

    Initial Commit

---
+++ scanner v3Tests/scanner_v3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  scanner_v3Tests.swift
+//  scanner v3Tests
+//
+//  Created by andrew bell on 31/12/2024.
+//
+
+import Testing
+@testable import scanner_v3
+
+struct scanner_v3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
